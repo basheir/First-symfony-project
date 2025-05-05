e74bf32c420674a6a32e734598601a743f1ad544
Bashir Mahad <basheirmahad@gmail.com>
2025-05-05 17:06:01 +0300

    Add: Form

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Form\ProductForm;
 
 final class ProductController extends AbstractController
 {
@@ -18,18 +20,30 @@ final class ProductController extends AbstractController
         ]);
     }
 
-    #[Route('/product/{id<\d+>}')]
-    public function show($id, ProductRepository $repository):Response 
+    #[Route('/product/{id<\d+>}', name: 'product_show')]
+    public function show(Product $product):Response 
     {
         
-        $product = $repository->findOneBy(['id' => $id]);
+        // $product = $repository->findOneBy(['id' => $id]);
 
-        if ($product === null) {
-            throw $this->createNotFoundException("Product not found");
-        }
+        // if ($product === null) {
+        //     throw $this->createNotFoundException("Product not found");
+        // }
 
         return $this->render('product/show.html.twig', [
             'product' => $product
+        ]);
+    }
+
+
+    #[Route('/product/new', name: 'product_new')]
+    public function new(): Response
+    {
+
+        $form = $this->createForm(ProductForm::class);
+
+        return $this->render('product/new.html.twig', [
+            'form' => $form,
         ]);
     }
 }
